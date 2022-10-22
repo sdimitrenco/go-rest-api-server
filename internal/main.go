c55d6d930ed6bcc165a383b759f17cfa25055b9c
stanislavdimitrenco <stanislav.dimitrenko@gmail.com>
2022-10-22 09:44:44 +0200

    updated main file

--- internal/main.go
+++ internal/main.go
@@ -2,17 +2,62 @@ package main
 
 import (
 	"fmt"
-	"html"
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/sdimitrenco/go-rest-api-server/pkg/swagger/server/restapi"
+	"github.com/sdimitrenco/go-rest-api-server/pkg/swagger/server/restapi/operations"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		_, _ = fmt.Fprintf(writer, "Hello world, %q ", html.EscapeString(request.URL.Path))
+	//Initialize Swagger
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	})
+	api := operations.NewHelloAPIAPI(swaggerSpec)
+	server := restapi.NewServer(api)
 
-	log.Println("Listening on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8088", nil))
+	defer func() {
+		if err := server.Shutdown(); err != nil {
+			// error handler
+			log.Fatalln(err)
+		}
+	}()
+
+	server.Port = 8080
+
+	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
+
+	//Start server with listening
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+//Health route returns OK
+func Health(params operations.CheckHealthParams) middleware.Responder {
+	return operations.NewCheckHealthOK().WithPayload("OK")
+}
+
+//GetGopherByName returns a gopher in png
+func GetGopherByName(gopher operations.GetGopherNameParams) middleware.Responder {
+	var URL string
+	if gopher.Name != "" {
+		URL = "https://github.com/scraly/gophers/raw/main/" + gopher.Name + ".png"
+	} else {
+		//by default we return dr who gopher
+		URL = "https://github.com/scraly/gophers/raw/main/dr-who.png"
+	}
+
+	response, err := http.Get(URL)
+
+	if err != nil {
+		fmt.Println("error ...")
+	}
+
+	return operations.NewGetGopherNameOK().WithPayload(response.Body)
+
 }
